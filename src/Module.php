56eb7929a0503f08c87f25fe9ac0e415009ca679
maxgu <gulturyan@t4web.com.ua>
2016-06-20 18:25:51 +0300

    add zendframework/zend-console dependency

--- src/Module.php
+++ src/Module.php
@@ -2,6 +2,8 @@
 
 namespace T4web\Queue;
 
+use Zend\Console\Adapter\AdapterInterface as Console;
+
 class Module
 {
     public function getAutoloaderConfig()
@@ -18,6 +20,14 @@ class Module
     public function getConfig()
     {
         return include dirname(__DIR__) . '/config/module.config.php';
+    }
+
+    public function getConsoleUsage(Console $console)
+    {
+        return array(
+            // Describe available commands
+            'queue realtime-server' => 'Run realtime queue server',
+        );
     }
 }
 
